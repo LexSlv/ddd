5540fe62dc7848af5c718983a22e2a7f6dfb6c3f
LexSlv <alexeyslivnev@gmail.com>
2021-02-18 19:50:18 +0600

    editor

--- controllers/FellowsController.php
+++ controllers/FellowsController.php
@@ -1,12 +1,41 @@
 <?php
 
 namespace app\controllers;
+use Yii;
+use app\models\Blocks;
+
 
 class FellowsController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $blocksModel = Blocks::find()->all();
+        $lang = Yii::$app->language;
+        foreach ($blocksModel as $block) {
+            if($block['key'] === "DKU_fellows"){
+                $blocksArr['DKU_fellows'] = $block['text_block_'.$lang];
+            }
+
+            if($block['key'] === "fellows_block_1"){
+                $blocksArr['fellows_block_1'] = $block['text_block_'.$lang];
+            }
+
+            if($block['key'] === "fellows_block_2"){
+                $blocksArr['fellows_block_2'] = $block['text_block_'.$lang];
+            }
+
+            if($block['key'] === "dku_senior_fellows"){
+                $blocksArr['dku_senior_fellows'] = $block['text_block_'.$lang];
+            }
+
+            if($block['key'] === "dku_junior_fellows"){
+                $blocksArr['dku_junior_fellows'] = $block['text_block_'.$lang];
+            }
+        }
+
+        return $this->render('index', [
+            'text_blocks' => $blocksArr
+        ]);
     }
 
 }
--- modules/admin/views/Blocks/_form.php
+++ modules/admin/views/Blocks/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use dosamigos\tinymce\TinyMce;
 /* @var $this yii\web\View */
 /* @var $model app\models\Blocks */
 /* @var $form yii\widgets\ActiveForm */
@@ -13,10 +14,43 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'key')->textInput(['maxlength' => true]) ?>
-
+    <!--
     <?= $form->field($model, 'text_block_ru')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'text_block_en')->textarea(['rows' => 6]) ?>
+    -->
+
+
+
+    <?= $form->field($model, 'text_block_ru')->widget(TinyMce::className(), [
+        'options' => ['rows' => 10],
+        'language' => 'ru',
+        'clientOptions' => [
+            'plugins' => [
+                "advlist autolink lists link charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste",
+                "image"
+            ],
+            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        ]
+    ]);?>
+
+
+    <?= $form->field($model, 'text_block_en')->widget(TinyMce::className(), [
+        'options' => ['rows' => 10],
+        'language' => 'ru',
+        'clientOptions' => [
+            'plugins' => [
+                "advlist autolink lists link charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste",
+                "image"
+
+            ],
+            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        ]
+    ]);?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
